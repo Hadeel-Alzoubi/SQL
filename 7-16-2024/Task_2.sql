e9b94c0874b3101a2fa1bf1799e255ae34334ba3
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-07-17 20:21:36 +0300

    Task 3

--- 7-16-2024/Task_2.sql
+++ 7-16-2024/Task_2.sql
@@ -9,6 +9,11 @@ T_ID int foreign key (T_ID) references Teacher(ID),
 S_ID int foreign key (S_ID) references Student (ID)
 );
 
+create table course_Studen (
+C_ID int foreign key (C_ID) references Course(ID),
+S_ID int foreign key (S_ID) references Student (ID)
+);
+
 create table Teacher_Cours(
 T_ID int foreign key (T_ID) references Teacher(ID),
 C_ID int foreign key (C_ID) references Course (ID)
---
+++ 7-17-2024/Task3.sql
@@ -0,0 +1,76 @@
+create database jaradat_Tech_solution;
+
+create table auther (ID int primary key IDENTITY(1,1), name varchar(20));
+create table section (ID int primary key IDENTITY(1,1),  name varchar(20));
+create table category (ID int primary key IDENTITY(1,1), name varchar(20));
+
+
+create table employee (ID int primary key IDENTITY(1,1), name varchar(20) , S_ID_fk int,
+foreign key (S_ID_fk) references section(ID)
+);
+create table users (ID int primary key IDENTITY(1,1), name varchar(20));
+
+create table book (
+ID int primary key IDENTITY(1,1), name varchar(20) ,
+A_ID_fk int,
+U_ID_fk int,
+S_ID_fk int,
+foreign key (A_ID_fk) references auther(ID),
+foreign key (U_ID_fk) references users(ID),
+foreign key (S_ID_fk) references section(ID)
+);
+
+--drop table book_category;
+
+create table book_category (id_book int,id_category int,
+primary key (id_book,id_category),
+foreign key (id_book)  references book(ID),
+foreign key (id_category) references category(ID)
+);
+
+--alter table book_category 
+--add id_book int;
+
+--alter table book_category
+--add id_category int;
+
+--This is Practic in 1 : M Relation
+select auther.name , book.name
+from auther
+join book on auther.ID = book.A_ID_fk;
+
+select auther.name , book.name
+from auther
+left join book on auther.ID = book.A_ID_fk;
+
+select  auther.name , book.name
+from auther
+right join book on auther.ID = book.A_ID_fk;
+
+select auther.name , book.name
+from auther
+full join book on auther.ID = book.A_ID_fk;
+
+
+--This is Practic in M : M Relation
+
+select book.ID, book.name, category.name
+from book
+join book_category on book.ID = book_category.id_book
+join category on book_category.id_category = category.ID;
+
+--select category.name , book.name
+--from category,book
+--join book_category on book_category.id_book = book_category.id_category;
+
+select auther.name , book.name
+from auther
+left join book on auther.ID = book.A_ID_fk;
+
+select  auther.name , book.name
+from auther
+right join book on auther.ID = book.A_ID_fk;
+
+select auther.name , book.name
+from auther
+full join book on auther.ID = book.A_ID_fk;
